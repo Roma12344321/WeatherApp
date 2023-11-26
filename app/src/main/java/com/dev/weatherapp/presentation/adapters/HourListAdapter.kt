1d010228a77dc8e0283e23812db0c11b4e89dd6d
romam <romamar2004@gmail.com>
2023-11-26 16:17:38 +0300

    HourListAdapter improvement

--- app/src/main/java/com/dev/weatherapp/presentation/adapters/HourListAdapter.kt
+++ app/src/main/java/com/dev/weatherapp/presentation/adapters/HourListAdapter.kt
@@ -7,6 +7,7 @@ import com.bumptech.glide.Glide
 import com.dev.weatherapp.R
 import com.dev.weatherapp.domain.Day
 import com.dev.weatherapp.domain.Hour
+import java.lang.RuntimeException
 
 class HourListAdapter : ListAdapter<Hour, HourViewHolder>(HourItemDiffCallBack()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourViewHolder {
@@ -16,11 +17,17 @@ class HourListAdapter : ListAdapter<Hour, HourViewHolder>(HourItemDiffCallBack()
 
     override fun onBindViewHolder(viewHolder: HourViewHolder, position: Int) {
         val hour = getItem(position)
-        viewHolder.textViewHourDate.text = hour.time
+        viewHolder.textViewHourDate.text = parseTime(hour)
         viewHolder.textViewHourDegree.text = hour.temperature.toString()
         viewHolder.textViewHourCondition.text = hour.textOfHourConditions
         Glide.with(viewHolder.itemView)
             .load("https:" + hour.iconOfHourConditions)
             .into(viewHolder.imageViewHourCondition)
+    }
+
+    private fun parseTime(hour : Hour): String {
+        val str = hour.time
+        val lastIndex = str!!.length - 5
+        return str.substring(lastIndex)
     }
 }
